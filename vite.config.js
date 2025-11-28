a958b9328b6ec6bdbdd5b0c09cfcd80d54974b88
kimdongho123 <tmzkdlxj480@gmail.com>
2025-11-28 09:59:02 +0900

    dd

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   // base: "/test/", // <--- 중요: 슬래시 꼭 포함
   plugins: [react()],
-  base: './',  // root에 배포할 때는 ./로 설정
+  base: '/React-To-do/',  // repo 이름 정확히
+  build: {
+    outDir: 'docs', // dist 대신 docs 폴더로 빌드
+  }
 });
